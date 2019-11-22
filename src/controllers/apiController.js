75d5f43b3e44cd4d779ec7cec34c134aeb9442e4
RobertSou <robertsouza915@gmail.com>
2019-11-22 13:34:58 -0300

    api fixed

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -2,7 +2,18 @@ const Ong = require('../models/ongModel');
 
 module.exports = {
   async showAllOngs(req, res){
-    const allOngs = await Ong.find();
-    return res.send(allOngs);
+    const allOngs = await Ong.find({});
+
+    let Ongs = [];
+
+    allOngs.map(donation => {
+      Ongs.push({
+        "name": donation.name,
+        "email": donation.email,
+        "publicInfo": donation.publicInfo,
+      });
+    });
+
+    return res.send(Ongs);
   }
 }
